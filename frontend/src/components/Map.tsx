916f0a644e01dd396f18a261e4015a27f85651bc
Baymax1705 <yashverma.off17@gmail.com>
2026-04-14 01:46:34 +0530

    Re-implemented GeoIntelligence Frontend natively in React.js (Vite)

---
+++ frontend/src/components/Map.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+function ChangeView({ center, zoom }: { center: [number, number], zoom: number }) {
+  const map = useMap();
+  useEffect(() => {
+    map.setView(center, zoom);
+  }, [center, zoom, map]);
+  return null;
+}
+
+export default function Map({ center = [26.8467, 80.9462], zoom = 14 }: { center: [number, number]; zoom?: number }) {
+    useEffect(() => {
+        // Fix Leaflet icons issue
+        delete (L.Icon.Default.prototype as any)._getIconUrl;
+        L.Icon.Default.mergeOptions({
+            iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+            iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+            shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+        });
+    }, []);
+
+    return (
+        <MapContainer center={center} zoom={zoom} style={{ height: '100%', width: '100%', zIndex: 0 }}>
+            <ChangeView center={center} zoom={zoom} />
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={center}>
+                <Popup>
+                    Selected Location
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+}
